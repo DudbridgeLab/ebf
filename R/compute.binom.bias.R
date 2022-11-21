6269d1818ca9610e978e0ef7bf5f8806ca80c4a8
Frank Dudbridge <frank.dudbridge@leicester.ac.uk>
2022-11-21 12:59:36 +0000

    Add files via upload

--- R/compute.binom.bias.R
+++ R/compute.binom.bias.R
@@ -27,9 +27,11 @@ compute.binom.bias <- function(n=1:100, shape=1) {
   for(i in n) {
 
     # find the maximum bias over the success probabilities
-    bias = c(bias, optimise(function(p) {
+    #bias = c(bias, optimise(function(p) {
+    bias = c(bias, integrate(function(p) {
       # observed data
       PML.obs = 0
+      PML.var = 0
       for(k.obs in 0:i) {
 
         # replicate data
@@ -37,18 +39,27 @@ compute.binom.bias <- function(n=1:100, shape=1) {
         for(k.rep in 0:i) {
           # posterior marginal likelihood
           PML.rep = PML.rep + dbinom(k.rep, i, p) *
-            (lchoose(i, k.rep) + lbeta(k.obs+k.rep+shape, 2*i-k.obs-k.rep+shape))
+            #(lchoose(i, k.rep) + lbeta(k.obs+k.rep+shape, 2*i-k.obs-k.rep+shape))
+            log(integrate(function(pp) dbinom(k.rep, i, pp) * dbeta(pp, k.obs+shape, i-k.obs+shape),
+                      0.3, 0.7)$value)
         }
 
         # the bias for this observed data
         PML.obs = PML.obs + dbinom(k.obs, i, p) *
+#          (lchoose(i, k.obs) + lbeta(2*k.obs+shape, 2*(i-k.obs)+shape) -
+#             PML.rep)
+        (log(integrate(function(pp) dbinom(k.obs, i, pp) * dbeta(pp, k.obs+shape, i-k.obs+shape),
+                       0.3, 0.7)$value) - PML.rep)
+        PML.var = PML.var + dbinom(k.obs, i, p) *
           (lchoose(i, k.obs) + lbeta(2*k.obs+shape, 2*(i-k.obs)+shape) -
-             PML.rep)
+             PML.rep)^2
       }
 
       # expectation over observed data
       PML.obs
-     }, c(0, 1), maximum=TRUE)$objective)
+      #PML.var - PML.obs^2
+     #}, c(0, 1), maximum=TRUE)$objective)
+    }, 0, 1)$value)
   }
 
   bias
