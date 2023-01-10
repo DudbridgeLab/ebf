75ffd0572c54bce75dd2d58df78e9777e0a54400
Frank Dudbridge <frank.dudbridge@leicester.ac.uk>
2023-01-10 17:12:09 +0000

    Delete beta.integral.R

--- R/beta.integral.R
+++
@@ -1,8 +0,0 @@
-# Integral of beta(1, b) density raised to power of npoints
-
-beta.integral <- function(x, npoints) {
-  pgamma(1, npoints+1, 1/x*npoints, lower=F)
-  #ntegrate(function(b) b^npoints * (1-x)^(npoints*(b-1)), 1, Inf)$value
-  #integrate(function(b) dbeta(x, npoints, npoints*b), 1, Inf)$value
-}
-
